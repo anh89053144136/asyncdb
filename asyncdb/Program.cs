f5ad1961c8407eb1d0da1c017ae6af5770920806
anh23 <anh23@rambler.ru>
2019-03-20 13:41:15 +0300

    settings

--- asyncdb/Program.cs
+++ asyncdb/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using Microsoft.Extensions.Configuration;
 using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
@@ -16,10 +18,17 @@ namespace asyncdb
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("Storage");
+            Console.WriteLine(connectionString);
 
             _sessionFactory = Fluently.Configure()
-             .Database(MsSqlConfiguration.MsSql2012.ConnectionString(@"Server=.;Database=asyncdb;Trusted_Connection=True;"))
+             .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<NHibernate.Cfg.Mappings>())
              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<asyncdb.Program>())
              .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(true, true))
@@ -27,14 +36,17 @@ namespace asyncdb
 
             ISession session = _sessionFactory.OpenSession();
 
-            DBFile newFile = new DBFile();
-            newFile.Name = "first file";
-            newFile.Content = File.ReadAllBytes("C:\\Users\\anh\\Desktop\\UXFSvywADvc.jpg");
+            //DBFile newFile = new DBFile();
+            //newFile.Name = "first file";
+            //newFile.Content = File.ReadAllBytes("C:\\Users\\anh\\Desktop\\UXFSvywADvc.jpg");
+            //session.SaveAsync(newFile);
+            //session.FlushAsync();
 
-            session.SaveAsync(newFile);
-            session.FlushAsync();
-
+            DBFile file = session.Query<DBFile>().FirstOrDefault();
             _sessionFactory.Close();
+
+            Console.WriteLine(file.Name);
+            Console.Read();
         }
     }
 }
